a5ec7c63d3a302367c49a239a888553db19c576f
Loucasse <takazyt@gmail.com>
2016-11-10 19:33:48 +0100

    Create script.user.js

---
+++ script.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name        Macros animateurs
+// @namespace   Macros de modérations pour les animateurs
+// @description Script qui permet d'ajouter des boutons de messages prédéfinis dans les forums
+// @include     https://realitygaming.fr/threads/*
+// @author      Hayzen
+// @version     1.0
+// @grant       none
+// ==/UserScript==
+
+$(document).ready(function(){
+    $.getScript('https://rawgit.com/LoucasseRG/macros_animateurs/master/script.js');
+});
